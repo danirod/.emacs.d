f1f019267c2f4ec0367b2a9d7e0756450f4e48a9
Dani Rodríguez <dani@danirod.es>
2021-10-13 16:30:36 +0200

    Update conf

--- init.el
+++ init.el
@@ -1,16 +1,3 @@
 ; bootstrap the real configuration file
 (require 'org)
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(treemacs-magit treemacs-projectile spinner lsp-mode use-package treemacs projectile nord-theme magit elcord)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
